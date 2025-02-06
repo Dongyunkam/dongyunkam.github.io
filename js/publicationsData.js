a284c77bacaa56e8dc2c6149ac842bb0f830f449
KamDongYun <rkaehddbs@gmail.com>
2025-02-06 13:55:12 +0900

    dykam

--- js/publicationsData.js
+++ js/publicationsData.js
@@ -1,26 +1,53 @@
 const publications = [
   {
-    imagePath: "img/thumbnails/sqattn.png",
-    title: "Squeezed Attention: Accelerating Long Context Length LLM Inference",
-    pdfLink: "https://arxiv.org/pdf/2411.09688.pdf",
+    imagePath: "img/thumbnails/panacea.png",
+    title: "Panacea: Novel DNN Accelerator using Accuracy-Preserving Asymmetric Quantization and Energy-Saving Bit-Slice Sparsity",
+    pdfLink: "https://arxiv.org/abs/2412.10059",
     codeLinks: {},
     authors:
-      "Coleman Hooper*, <b>Sehoon Kim*</b>, Hiva Mohammadzadeh, Monishwaran Maheswaran, June Paik, Michael W. Mahoney, Kurt Keutzer, Amir Gholami",
-    conference: "Preprint, 2024",
+      "<b>Dongyun Kam</b>, Myeongji Yun, Sunwoo Yoo, Seungwoo Hong, Zhengya Zhang, Youngjoo Lee",
+    conference: "Accepted by HPCA, 2025",
     selected: true,
   },
   {
-    title: "TinyAgent: Function Calling at the Edge",
-    pdfLink: "https://arxiv.org/pdf/2409.00608",
-    codeLinks: {
-      Code: "https://github.com/SqueezeAILab/TinyAgent",
-      Blog: "https://bair.berkeley.edu/blog/2024/05/29/tiny-agent/",
-    },
+    imagePath: "img/thumbnails/boss.png",
+    title: "A 21.9 ns 15.7 Gbps/mm² (128, 15) BOSS FEC Decoder for 5G/6G URLLC Applications",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/10454363",
+    codeLinks: {},
     authors:
-      "Lutfi Eren Erdogan*, Nicholas Lee*, Siddharth Jha*, <b>Sehoon Kim</b>, Ryan Tabrizi, Suhong Moon, Coleman Hooper, Gopala Anumanchipalli, Kurt Keutzer, Amir Gholami",
-    conference:
-      "Empirical Methods in Natural Language Processing (EMNLP) Demo Track, 2024",
-    selected: false,
+      "<b>Dongyun Kam</b>, Sangbu Yun, Jeongwon Choe, Zhengya Zhang, Namyoon Lee, Youngjoo Lee",
+    conference: "IEEE International Solid-State Circuits Conference (ISSCC), 2024",
+    selected: true,
+  },
+  {
+    imagePath: "img/thumbnails/overlap_polar.png",
+    title: "Low-latency SCL polar decoder architecture using overlapped pruning operations",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/10014433",
+    codeLinks: {},
+    authors:
+      "<b>Dongyun Kam</b>, Byeong Yong Kong, Youngjoo Lee",
+    conference: "IEEE Transactions on Circuits and Systems I: Regular Papers (TCAS-I), 2023",
+    selected: true,
+  },
+  {
+    imagePath: "img/thumbnails/ART-9.png",
+    title: "Design and Evaluation Frameworks for Advanced RISC-based Ternary Processor",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/9774584",
+    codeLinks: {},
+    authors:
+      "<b>Dongyun Kam*</b>, Jung Gyu Min, Jongho Yoon, Sunmean Kim, Seokhyeong Kang, Youngjoo Lee",
+    conference: "Design, Automation & Test in Europe Conference & Exhibition (DATE), 2022",
+    selected: true,
+  },
+ {
+    imagePath: "img/thumbnails/Mem_polar.png",
+    title: "A 1.1μs 1.56Gb/s/mm2 Cost-Efficient Large-List SCL Polar Decoder Using Fully-Reusable LLR Buffers in 28nm CMOS Technology",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/9830317",
+    codeLinks: {},
+    authors:
+      "<b>Dongyun Kam</b>, Byeong Yong Kong, Youngjoo Lee",
+    conference: "IEEE Symposium on VLSI Technology and Circuits (VLSI Technology and Circuits), 2022",
+    selected: true,
   },
 ];
 
